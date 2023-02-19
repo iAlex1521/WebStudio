e4386936af162a37b6b2973fda39a154ff2fbbd9
Alex <4it2me2021@gmail.com>
2023-02-19 22:55:03 +0200

    update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,32 +1,58 @@
-// // const filterBox = document.querySelectorAll('.box');
-
-// document.querySelector('.filters').addEventListener('click', event => {
-//     if (event.target.tagName !== 'LI') return false;
-
-//     let filterClass = event.target.dataset['f'];
-//     console.log(filterClass);
-// });
-
-// const list = document.querySelector('.filters'),
-//     items = document.querySelectorAll('.box');
-
-// function filter() {
-//     list.addEventListener('click', event => {
-//         let targetId = event.target.dataset['id'];
-//         console.log(targetId);
-//     });
-
-// }
-// filter();
-
 function app() {
     const buttons = document.querySelectorAll('.filters-button')
-
+    const filterBox = document.querySelectorAll('.box')
+        
     buttons.forEach((elem) => {
         elem.addEventListener('click', () => {
-            console.log(elem.parentElement.dataset.f)
+            const targetId = elem.parentElement.dataset.f
+            console.log(targetId)
+
+            switch (targetId) {
+                case 'all':
+                    filterBox.forEach(item => {
+                        if (item.classList.contains('box')) {
+                            item.style.display = 'block'
+                        } 
+                    })
+                    break
+                case 'web-site':
+                    filterBox.forEach(item => {
+                        if (item.classList.contains('web-site')) {
+                            item.style.display = 'block'
+                        } else {
+                            item.style.display = 'none'
+                        }
+                    })
+                    break
+                case 'design':
+                    filterBox.forEach(item => {
+                        if (item.classList.contains('design')) {
+                            item.style.display = 'block'
+                        } else {
+                            item.style.display = 'none'
+                        }
+                    })
+                    break
+                case 'application':
+                    filterBox.forEach(item => {
+                        if (item.classList.contains('application')) {
+                            item.style.display = 'block'
+                        } else {
+                            item.style.display = 'none'
+                        }
+                    })
+                    break
+                case 'marketing':
+                    filterBox.forEach(item => {
+                        if (item.classList.contains('marketing')) {
+                            item.style.display = 'block'
+                        } else {
+                            item.style.display = 'none'
+                        }
+                    })
+                    break
+            }
         })
     })
 }
-
 app()
